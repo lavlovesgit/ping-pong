3bb10768ad02e4b7ad6d91d1612596dcdee4f6f1
lavlovesgit <148366150+lavlovesgit@users.noreply.github.com>
2025-11-18 18:37:26 +0530

    Update ball.py to make it work properprly updated all functions

--- game/ball.py
+++ game/ball.py
@@ -2,7 +2,8 @@ import pygame
 import random
 
 class Ball:
-    def __init__(self, x, y, width, height, screen_width, screen_height):
+    def __init__(self, x, y, width, height, screen_width, screen_height,
+                 sound_wall=None, sound_paddle=None, sound_score=None):
         self.original_x = x
         self.original_y = y
         self.x = x
@@ -14,16 +15,31 @@ class Ball:
         self.velocity_x = random.choice([-5, 5])
         self.velocity_y = random.choice([-3, 3])
 
-    def move(self):
+        self.sound_wall = sound_wall
+        self.sound_paddle = sound_paddle
+        self.sound_score = sound_score
+
+    def move(self, player, ai):
         self.x += self.velocity_x
         self.y += self.velocity_y
 
+        # Bounce off top/bottom
         if self.y <= 0 or self.y + self.height >= self.screen_height:
             self.velocity_y *= -1
+            if self.sound_wall:
+                self.sound_wall.play()
 
-    def check_collision(self, player, ai):
-        if self.rect().colliderect(player.rect()) or self.rect().colliderect(ai.rect()):
+        # Paddle collision
+        if self.rect().colliderect(player.rect()):
+            self.x = player.x + player.width
             self.velocity_x *= -1
+            if self.sound_paddle:
+                self.sound_paddle.play()
+        elif self.rect().colliderect(ai.rect()):
+            self.x = ai.x - self.width
+            self.velocity_x *= -1
+            if self.sound_paddle:
+                self.sound_paddle.play()
 
     def reset(self):
         self.x = self.original_x
